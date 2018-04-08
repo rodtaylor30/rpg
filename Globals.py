58c98a51c938d7a422b85ca1074adfc919b3c250
rodtaylor30 <sven674@web.de>
2018-04-08 16:50:01 +0200

    Create Globals.py

---
+++ Globals.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python.
+
+"""
+    This file defines the globals
+
+"""
+
+__author__ = "Sven Eggert"
+__copyright__ = "Copyright 2018, Egertiko Designs"
+__credits__ = []
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Sven Eggert"
+__email__ = ""
+__status__ = "Development"
+
+"""
+
+    The global constants
+    
+"""
+
+WALL_NORTH = 1
+WALL_SOUTH = 2
+WALL_WEST = 4
+WALL_EAST = 8
+
+GONE_EAST = 1
+GONE_SOUTH = 2
+GONE_WEST = 4
+GONE_NORTH = 8
+GONE_STOP = 8
+GONE_ALL = 15
+
+MAZE_SIZE_X = 10
+MAZE_SIZE_Y = 10
+MIN_MAZE_SIZE_X = 1
+MIN_MAZE_SIZE_Y = 1
+
+LEAVE_OUT_X_1 = 0
+LEAVE_OUT_X_2 = 1
+LEAVE_OUT_Y_1 = 2
+LEAVE_OUT_Y_2 = 3
